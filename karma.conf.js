81454252c246996c3867a3d63235827906a0204a
Nemanja Petrovic <nemanja.petrovic@badin.rs>
2020-12-15 14:59:54 +0100

    IT255_DZ14

--- karma.conf.js
+++ karma.conf.js
@@ -1,36 +1,21 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function (config) {
-  config.set({
+module.exports = function(config) {
+  let configuration = {
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage'),
+      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // ostavlja Jasmine Spec Runner output vidljiv u pregledaču
     },
-    jasmineHtmlReporter: {
-      suppressAll: true // removes the duplicated traces
-    },
-    coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/met-youtube'),
-      subdir: '.',
-      reporters: [
-        { type: 'html' },
-        { type: 'text-summary' }
-      ]
+    coverageIstanbulReporter: {
+      dir: require('path').join(__dirname, '../coverage'),
+      reports: ['html', 'lcovonly'],
+      fixWebpackSourcePaths: true
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
@@ -38,7 +23,19 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
+    singleRun: false
+  };
+ 
+  if (process.env.TRAVIS) {
+    configuration.customLaunchers = {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    };
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+ 
+  config.set(configuration);
 };
+
--- src/app/services/youtube-search.service.spec.ts
+++ src/app/services/youtube-search.service.spec.ts
@@ -6,11 +6,40 @@ describe('YoutubeSearchService', () => {
   let service: YoutubeSearchService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(YoutubeSearchService);
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          BaseRequestOptions,
+          MockBackend,
+          YoutubeSearchService,
+          { provide: Http,
+            useFactory: (backend: ConnectionBackend,
+                         defaultOptions: BaseRequestOptions) => {
+                           return new Http(backend, defaultOptions);
+                         }, deps: [MockBackend, BaseRequestOptions] },
+        ]
+      });
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should should search', () => {
+    inject([YoutubeSearchService, MockBackend], fakeAsync((svc, backend) => {
+      let res;
+      expectURL(backend, 'https://www.googleapis.com/youtube/v3/search');
+      svc.getBody('title').subscribe((_res) => {
+        res = _res;
+      });
+      tick();
+  });
+
+  function expectURL(backend: MockBackend, url: string) {
+    backend.connections.subscribe(c => {
+      expect(c.request.url).toBe(url);
+      const response = new ResponseOptions({body: '{"title": "Drzavni posao 222"}'});
+      c.mockRespond(new Response(response));
+    });
+  }
 });
